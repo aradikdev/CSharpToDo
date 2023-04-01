bfd80960db08b602fc6cddcc57ee66db97ffd67b
aradikdev <aradikdev@gmail.com>
2023-04-01 10:34:31 +0600

    Add project files.

---
+++ TODO.Web/Data/TODOContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TODO.Web.Data;
+
+public class TODOContext : DbContext
+{
+    public TODOContext(DbContextOptions options) : base(options)
+    {
+    }
+
+}
